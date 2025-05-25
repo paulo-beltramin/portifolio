694583af84ce6e5ff04c94423acb05d8ad89372e
paulo-beltramin <dev.beltramin@gmail.com>
2025-05-25 11:38:09 -0300

    finalizado layout e responsividade do component header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,33 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export const Header = () => {
+
+
   return (
+
     <>
-      <header>
-        
-      </header>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+
+          <Navbar.Brand href="/" className="flex justify-between w-full items-center">
+            <div>
+              <span className="text-4xl font-extrabold max-md:text-lg max-md:font-bold">
+                Web
+              </span>
+              <span className="text-indigo-100 text-3xl max-md:text-sm font-bold">
+                Developer
+              </span>
+            </div>
+            <div>
+              <Nav className="me-auto text-lg font-bold">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/projetos">Projetos</Nav.Link>
+              </Nav>
+            </div>
+          </Navbar.Brand>
+
+        </Container>
+      </Navbar >
     </>
   )
 }
