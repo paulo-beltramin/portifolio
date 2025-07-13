6be077cc37b6c7b046c68deeb52751d5571c9368
paulo-beltramin <dev.beltramin@gmail.com>
2025-07-13 13:12:31 -0300

    new build 2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router";
 import { Home } from "../pages/Home";
 import { Layout } from "../Components/Layout";
 import { Projects } from "../pages/Projects";
-import { Login } from "../pages/Login";
 
 
 
